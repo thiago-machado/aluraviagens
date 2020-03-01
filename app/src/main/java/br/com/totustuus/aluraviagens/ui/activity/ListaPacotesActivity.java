9dbbe5e6533e4071944e9c9843b4d28957015927
Thiago Machado <ti-thiago@outlook.com>
2020-03-01 17:44:13 -0300

    Finalização da aula 1 do curso de Constraints Layouts módulo 1

---
+++ app/src/main/java/br/com/totustuus/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package br.com.totustuus.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.totustuus.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
