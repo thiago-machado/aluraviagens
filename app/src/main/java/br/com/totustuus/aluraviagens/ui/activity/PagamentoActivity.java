10f3fe92dc615eb3e9368767e466f01eeaf26a36
Thiago Machado <ti-thiago@outlook.com>
2020-03-04 08:53:59 -0300

    Finalizada Aula 3 do curso de Layout com Constraints Módulo 2

--- app/src/main/java/br/com/totustuus/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/totustuus/aluraviagens/ui/activity/PagamentoActivity.java
@@ -2,14 +2,31 @@ package br.com.totustuus.aluraviagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.totustuus.aluraviagens.R;
+import br.com.totustuus.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        mostraPreco();
+    }
+
+    private void mostraPreco() {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil
+                .formataParaReal(new BigDecimal("123.99"));
+        preco.setText(moedaBrasileira);
     }
 }
